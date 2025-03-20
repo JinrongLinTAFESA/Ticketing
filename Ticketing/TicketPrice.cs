67ab6cbc7a849ef7127d2041b621de55b95efd13
001225474 <jinrong.lin@student.tafesa.edu.au>
2025-03-20 20:26:49 +1000

    2nd changing ticket price

--- Ticketing/TicketPrice.cs
+++ Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 20m;
-        const decimal mdecGeneral = 15m;
-        const decimal mdecBox = 10m;
+        const decimal mdecBalcony = 25m;
+        const decimal mdecGeneral = 20m;
+        const decimal mdecBox = 15m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
